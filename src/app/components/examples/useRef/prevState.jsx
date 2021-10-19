61f4c45e849f760fb2a95a9c2732f4ad90a9bfc1
Mokhmad <mokhmad.tazbiev@gmail.com>
2021-10-19 17:34:51 +0300

    add useRef

--- src/app/components/examples/useRef/prevState.jsx
+++ src/app/components/examples/useRef/prevState.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import CardWrapper from "../../common/Card";
 import SmallTitle from "../../common/typografy/smallTitle";
 const PrevStateExample = () => {
+    const prevState = useRef("");
+    const [otherState, setOtherState] = useState("false");
+    const toogleOther = () => {
+        setOtherState((prevState) =>
+            prevState === "false" ? "true" : "false"
+        );
+    };
+    useEffect(() => {
+        prevState.current = otherState;
+    }, [otherState]);
+
     return (
         <CardWrapper>
             <SmallTitle>Предыдущее состояние</SmallTitle>
+            <p>Prev State: {prevState.current}</p>
+            <p>Current State: {otherState}</p>
+            <button onClick={toogleOther} className="btn btn-primary">
+                Toogle other state
+            </button>
         </CardWrapper>
     );
 };
--- src/app/components/examples/useRef/renderCount.jsx
+++ src/app/components/examples/useRef/renderCount.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import CardWrapper from "../../common/Card";
 import SmallTitle from "../../common/typografy/smallTitle";
 const RenderCountExample = () => {
+    const renderCount = useRef(0);
+    const [otherState, setOtherState] = useState(false);
+    const toogleOther = () => {
+        setOtherState(!otherState);
+    };
+    useEffect(() => {
+        renderCount.current++;
+    });
+
     return (
         <CardWrapper>
             <SmallTitle>Подсчет количесва рендеров</SmallTitle>
+            <p>renderCount: {renderCount.current}</p>
+            <button onClick={toogleOther} className="btn btn-primary">
+                Toogle other state
+            </button>
         </CardWrapper>
     );
 };
