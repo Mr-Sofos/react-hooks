7c8b0f49236b996da537a088616c89957e72ef63
Mokhmad <mokhmad.tazbiev@gmail.com>
2021-10-28 18:53:01 +0300

    fix

--- src/app/components/exercises/useRefExercise.jsx
+++ src/app/components/exercises/useRefExercise.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useRef } from "react";
 import CollapseWrapper from "../common/collapse";
 const UseRefExercise = () => {
+    const blockRef = useRef();
+    const handleClick = () => {
+        console.log(blockRef);
+        blockRef.current.innerHTML = "Text";
+        blockRef.current.style.height = "150px";
+        blockRef.current.style.width = "80px";
+        blockRef.current.style.backgroundColor = "red";
+    };
     return (
         <CollapseWrapper title="Упражнение">
             <p className="mt-3">
@@ -11,15 +19,20 @@ const UseRefExercise = () => {
                 <li>Изменисят содежимое блока на &quot;text&quot;</li>
                 <li>высота и ширина станут равны 150 и 80 соответственно</li>
             </ul>
-            <div
-                className="bg-primary d-flex flex-row justify-content-center align-items-center rounded"
-                style={{
-                    height: 40,
-                    width: 60,
-                    color: "white"
-                }}
-            >
-                <small>Блок</small>
+            <div className="d-flex">
+                <div
+                    className="bg-primary d-flex flex-row justify-content-center align-items-center rounded"
+                    style={{
+                        height: 40,
+                        width: 60,
+                        color: "white"
+                    }}
+                >
+                    <small ref={blockRef}>Блок</small>
+                </div>
+                <button className="btn btn-primary mx-2" onClick={handleClick}>
+                    Кнопка
+                </button>
             </div>
         </CollapseWrapper>
     );
